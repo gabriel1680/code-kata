8d7c39c20bbe806f1a48d871470c773c9dbc8c11
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2023-07-29 15:32:47 -0300

    chore: setup config

---
+++ fizz-buzz/fizz-buzz.spec.ts
@@ -0,0 +1,6 @@
+describe("FizzBuzz", () => {
+    it("should return 'Fizz' when n is divisible by 3");
+    it("should return 'Buzz' when n is divisible by 5");
+    it("should return 'FizzBuzz' when n is divisible by 3 and 5");
+    it("should return the number 'n' when n is neither divisible by 3 nor 5");
+});
--- fizz-buzz/vitest.config.ts
+++ fizz-buzz/vitest.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {
-    globals: true,
-  },
+    test: {
+        globals: true,
+        testTimeout: 3000,
+        include: ['*.spec.ts'],
+        coverage: {
+            all: true,
+            provider: 'istanbul',
+        },
+    },
 })
