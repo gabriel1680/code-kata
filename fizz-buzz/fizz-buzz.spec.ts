96b4db701db8a92b18f73fb2669d1455b775d104
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2023-07-29 15:41:59 -0300

    feat: fizzbuzz first impl

--- fizz-buzz/fizz-buzz.spec.ts
+++ fizz-buzz/fizz-buzz.spec.ts
@@ -1,6 +1,19 @@
+import { fizzbuzz } from "fizz-buzz";
+
 describe("FizzBuzz", () => {
-    it("should return 'Fizz' when n is divisible by 3");
-    it("should return 'Buzz' when n is divisible by 5");
-    it("should return 'FizzBuzz' when n is divisible by 3 and 5");
-    it("should return the number 'n' when n is neither divisible by 3 nor 5");
+    it("should return 'Fizz' when n is divisible by 3", () => {
+        expect(fizzbuzz(3)).toBe('Fizz');
+    });
+
+    it("should return 'Buzz' when n is divisible by 5", () => {
+        expect(fizzbuzz(5)).toBe('Buzz');
+    });
+
+    it("should return 'FizzBuzz' when n is divisible by 3 and 5", () => {
+        expect(fizzbuzz(15)).toBe('FizzBuzz');
+    });
+
+    it("should return the number 'n' when n is neither divisible by 3 nor 5", () => {
+        expect(fizzbuzz(1)).toBe(1);
+    });
 });
---
+++ fizz-buzz/fizz-buzz.ts
@@ -0,0 +1,10 @@
+export function fizzbuzz(n: number): string | number {
+    let result = '';
+    if (n % 3 === 0) {
+        result += 'Fizz';
+    }
+    if (n % 5 === 0) {
+        result += 'Buzz';
+    }
+    return result || n;
+}
