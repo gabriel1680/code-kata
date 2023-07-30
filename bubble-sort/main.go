e9d99b429da6d436b67aa1ee8b2cf044040c1826
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2023-07-30 12:22:33 -0300

    reafactor(bubble-sort): add swap function

--- bubble-sort/main.go
+++ bubble-sort/main.go
@@ -4,12 +4,16 @@ func BubbleSort(n []int) []int {
     for i := 0; i < len(n) - 1; i++ {
         for j := 0; j < len(n) - i - 1; j++ {
             if n[j] > n[j + 1] {
-                temp := n[j]
-                n[j] = n[j + 1]
-                n[j + 1] = temp
+                swap(&n, j, j + 1)
             }
         }
     }
     return n
+}
+
+func swap(n *[]int, left int, right int) {
+    aux := (*n)[left]
+    (*n)[left] = (*n)[right]
+    (*n)[right] = aux
 }
 
