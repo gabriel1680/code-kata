f5c996e8d6d6c616b0326ad539b098150df0ec2b
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2023-08-13 21:28:22 -0300

    fix: add edge cases for 40, 90, 400, 900

--- roman-numerals/src/main.rs
+++ roman-numerals/src/main.rs
@@ -18,7 +18,7 @@ pub fn to_roman(mut n: u32) -> String {
         for &i in keys.to_owned() {
             while n >= i {
                 let value = map.get(&i).unwrap();
-                roman.push(*value);
+                roman.push_str(value);
                 n -= i;
 
                 if map.contains_key(&(n + 1)) {
@@ -36,18 +36,22 @@ pub fn to_roman(mut n: u32) -> String {
     return roman;
 }
 
-fn create_roman_map() -> HashMap<u32, char> {
-    let mut map: HashMap<u32, char> = HashMap::new();
-    map.insert(5, 'V');
-    map.insert(10, 'X');
-    map.insert(50, 'L');
-    map.insert(100, 'C');
-    map.insert(500, 'D');
-    map.insert(1000, 'M');
+fn create_roman_map() -> HashMap<u32, String> {
+    let mut map: HashMap<u32, String> = HashMap::new();
+    map.insert(5, String::from("V"));
+    map.insert(10, String::from("X"));
+    map.insert(40, String::from("XL"));
+    map.insert(50, String::from("L"));
+    map.insert(90, String::from("XC"));
+    map.insert(100, String::from("C"));
+    map.insert(400, String::from("CD"));
+    map.insert(500, String::from("D"));
+    map.insert(900, String::from("CM"));
+    map.insert(1000, String::from("M"));
     map
 }
 
-fn get_reverse_sorted_keys_of(map: &HashMap<u32, char>) -> Vec<&u32> {
+fn get_reverse_sorted_keys_of(map: &HashMap<u32, String>) -> Vec<&u32> {
     let mut keys: Vec<&u32> = map.keys().collect();
     keys.sort();
     keys.reverse();
@@ -86,12 +90,16 @@ mod tests {
         assert_eq!("XXIV", to_roman(24));
         assert_eq!("XXV", to_roman(25));
         assert_eq!("L", to_roman(50));
+        assert_eq!("LXX", to_roman(70));
         assert_eq!("C", to_roman(100));
+        assert_eq!("CCXCIV", to_roman(294));
         assert_eq!("ID", to_roman(499));
         assert_eq!("D", to_roman(500));
         assert_eq!("DI", to_roman(501));
+        assert_eq!("DCC", to_roman(700));
         assert_eq!("M", to_roman(1000));
         assert_eq!("MM", to_roman(2000));
+        assert_eq!("MMXIX", to_roman(2019));
     }
 
 }
