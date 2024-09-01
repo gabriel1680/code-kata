b2a5e48ed24c7963150075beda85156c45d44ce1
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-09-01 17:00:38 -0300

    feat: add wardrobe possibilities calculator algorithm

--- wardrobe/src/main/java/com/wordtyping/WardrobePossibilities.java
+++ wardrobe/src/main/java/com/wordtyping/WardrobePossibilities.java
@@ -4,44 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WardrobePossibilities {
-    private final List<Integer> availableMeasures;
+    private final int[] availableMeasures;
 
     public WardrobePossibilities(List<Integer> someMeasures) {
-        availableMeasures = someMeasures;
+        availableMeasures = someMeasures.stream().mapToInt(Integer::intValue).toArray();
     }
 
-    public List<List<Integer>> getPossibilitiesFor(final int size) {
+    public List<List<Integer>> getPossibilitiesFor(final int target) {
         List<List<Integer>> result = new ArrayList<>();
-        for (var i = 0; i < availableMeasures.size(); i++) {
-            final var availableMeasure = availableMeasures.get(i);
-            iterateForSubValues(size, i, availableMeasure, result);
-        }
+        findCombinations(0, target, result, new ArrayList<>());
         return result;
     }
 
-    private void iterateForSubValues(int size, int i, Integer availableMeasure, List<List<Integer>> result) {
-        for (var j = i; j >= 0; j--) {
-            final var actual = new ArrayList<Integer>();
-            if (tryReduceToZero(availableMeasure, size, j, actual) == 0) {
-                result.add(actual);
-            }
+    private void findCombinations(int idx, int target, List<List<Integer>> result, List<Integer> ds) {
+        if (idx == availableMeasures.length) {
+            if (target == 0) result.add(new ArrayList<>(ds));
+            return;
         }
-    }
-
-    private int tryReduceToZero(Integer availableMeasure, int value, int j, ArrayList<Integer> actual) {
-        while (value > 0) {
-            if (j == 0) {
-                actual.add(availableMeasure);
-                value -= availableMeasure;
-            } else if ((value - availableMeasure) > availableMeasures.get(j - 1)) {
-                actual.add(availableMeasure);
-                value -= availableMeasure;
-            } else {
-                var prev = availableMeasures.get(j - 1);
-                actual.add(prev);
-                value -= prev;
-            }
+        if (availableMeasures[idx] <= target) {
+            ds.add(availableMeasures[idx]);
+            findCombinations(idx, target - availableMeasures[idx], result, ds);
+            ds.remove(ds.size() - 1);
         }
-        return value;
+        findCombinations(idx + 1, target, result, ds);
     }
 }
--- wardrobe/src/test/java/com/wordtyping/WardrobePossibilitiesTest.java
+++ wardrobe/src/test/java/com/wordtyping/WardrobePossibilitiesTest.java
@@ -13,9 +13,10 @@ class WardrobePossibilitiesTest {
         var sut = new WardrobePossibilities(wardrobes);
         final var possibilities = List.of(
             List.of(50, 50, 50, 50, 50),
-            List.of(75, 75, 50, 50),
-            List.of(100, 75, 75),
-            List.of(100, 50, 50, 50)
+            List.of(50, 50, 50, 100),
+            List.of(50, 50, 75, 75),
+            List.of(50, 100, 100),
+            List.of(75, 75, 100)
         );
         assertEquals(possibilities, sut.getPossibilitiesFor(250));
     }
