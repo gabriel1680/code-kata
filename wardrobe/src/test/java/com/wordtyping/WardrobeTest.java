818e1c9fcddf84b61665b8abfba8560dd9697625
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-09-02 22:36:51 -0300

    test: parametrize tests

--- wardrobe/src/test/java/com/wordtyping/WardrobeTest.java
+++ wardrobe/src/test/java/com/wordtyping/WardrobeTest.java
@@ -2,9 +2,13 @@ package com.wordtyping;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,20 +27,26 @@ class WardrobeTest {
         sut = new Wardrobe(measuresAndPrices);
     }
 
-    @Test
-    void getCombinations() {
-        final var possibilities = List.of(
+    private static Stream<Arguments> makeWardrobesConfigurations() {
+        final var wardrobeConfigurations = List.of(
             List.of(50, 50, 50, 50, 50), // 295
             List.of(50, 50, 50, 100), // 267
             List.of(50, 50, 75, 75), // 242
             List.of(50, 100, 100), // 239
             List.of(75, 75, 100) // 214
         );
-        assertEquals(possibilities, sut.getCombinationsFor(250));
+        return Stream.of(Arguments.of(wardrobeConfigurations));
     }
 
-    @Test
-    void getCheapestCombination() {
-        assertEquals(List.of(75, 75, 100), sut.getCheapestFor(250));
+    @ParameterizedTest
+    @MethodSource("makeWardrobesConfigurations")
+    void getCombinations(List<List<Integer>> configurations) {
+        assertEquals(configurations, sut.getCombinationsFor(250));
+    }
+
+    @ParameterizedTest
+    @MethodSource("makeWardrobesConfigurations")
+    void getCheapestCombination(List<List<Integer>> configurations) {
+        assertEquals(configurations.get(4), sut.getCheapestFor(250));
     }
 }
