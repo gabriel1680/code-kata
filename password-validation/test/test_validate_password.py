85d951e3a3fd18c3748908cba586ce5ba91005ba
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-09-08 00:24:06 -0300

    refactor: extract modules

--- password-validation/test/test_validate_password.py
+++ password-validation/test/test_validate_password.py
@@ -1,6 +1,4 @@
-import re
-from typing import Callable
-
+from src.validate_password import validate_password
 
 def test_password_should_not_have_less_than_8_char():
     password = "xxxxxxx"
@@ -29,38 +27,4 @@ def test_password_should_have_at_least_one_underscore():
 def test_password_should_be_ok():
     password = "Password4_8"
     assert True == validate_password(password)
-
-# ===== impl ====
-
-def have_more_than_8_len(password: str) -> bool:
-    return len(password) >= 8
-
-def contains(pattern: str, text: str) -> bool:
-    return re.search(pattern, text) is not None
-
-def have_lower_letter(password: str) -> bool:
-    return contains("[a-z]", password)
-
-def have_upper_letter(password: str) -> bool:
-    return contains("[A-Z]", password)
-
-def have_number(password: str) -> bool:
-    return contains("[0-9]", password)
-
-def have_underscore(password: str) -> bool:
-    return contains("[_]", password)
-
-validators = [
-    have_more_than_8_len,
-    have_lower_letter,
-    have_upper_letter,
-    have_number,
-    have_underscore
-]
-
-def validate_password(password: str) -> bool:
-    for rule in validators:
-        if rule(password) == False:
-            return False
-    return True
 
