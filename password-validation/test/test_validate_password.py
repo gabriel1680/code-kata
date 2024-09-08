86d2b61bdba3e1167bb8102d9d0644daa967ffeb
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-09-08 00:28:02 -0300

    fixup! fixup! test: move test

--- password-validation/test/test_validate_password.py
+++ password-validation/test/test_validate_password.py
@@ -2,29 +2,29 @@ from src.validate_password import validate_password
 
 def test_password_should_not_contains_only_lower_case_letters():
     password = "xxxxxxxx"
-    assert False == validate_password(password)
+    assert False is validate_password(password)
   
 def test_password_should_not_contains_only_upper_case_letters():
     password = "XXXXXXXX"
-    assert False == validate_password(password)
+    assert False is validate_password(password)
   
 def test_password_should_have_at_least_one_number():
     password = "xxxXXXxx"
-    assert False == validate_password(password)
+    assert False is validate_password(password)
 
 def test_password_should_have_at_least_one_letter():
     password = "111111111"
-    assert False == validate_password(password)
+    assert False is validate_password(password)
 
 def test_password_should_have_at_least_one_underscore():
     password = "Xx1xxxxx"
-    assert False == validate_password(password)
+    assert False is validate_password(password)
 
 def test_password_should_not_have_less_than_8_char():
     password = "Xxx4xx_"
-    assert False == validate_password(password)
+    assert False is validate_password(password)
 
 def test_password_should_be_ok():
     password = "Password4_8"
-    assert True == validate_password(password)
+    assert validate_password(password)
 
