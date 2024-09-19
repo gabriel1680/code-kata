20abb6fa71b075c112e140930ec20238f2f7d284
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-09-19 20:49:35 -0300

    feat: empty stack

---
+++ stack/src/main/java/com/kata/Stack.java
@@ -0,0 +1,7 @@
+package com.kata;
+
+public class Stack {
+    public boolean isEmpty() {
+        return true;
+    }
+}
---
+++ stack/src/test/java/com/kata/StackTest.java
@@ -0,0 +1,14 @@
+package com.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StackTest {
+
+    @Test
+    void newStackShouldBeEmpty() {
+        Stack stack = new Stack();
+        assertTrue(stack.isEmpty());
+    }
+}
