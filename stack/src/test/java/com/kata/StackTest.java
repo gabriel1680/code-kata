f69c810baf6f5f8d02430ea4ea95d425e5162a83
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-09-19 20:55:57 -0300

    refactor: tests

--- stack/src/test/java/com/kata/StackTest.java
+++ stack/src/test/java/com/kata/StackTest.java
@@ -1,21 +1,27 @@
 package com.kata;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StackTest {
 
+    private Stack stack;
+
+    @BeforeEach
+    void setUp() {
+        stack = new Stack();
+    }
+
     @Test
     void newStackShouldBeEmpty() {
-        Stack stack = new Stack();
         assertTrue(stack.isEmpty());
         assertEquals(0, stack.size());
     }
 
     @Test
     void pushOneNotEmpty() {
-        Stack stack = new Stack();
         stack.push(1);
         assertEquals(1, stack.size());
         assertFalse(stack.isEmpty());
