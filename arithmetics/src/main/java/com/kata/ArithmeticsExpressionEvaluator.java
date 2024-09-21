fcca87b405e0d4115f55c995ed0216a8493bb6c5
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-09-21 17:50:07 -0300

    feat: simplify validations

--- arithmetics/src/main/java/com/kata/ArithmeticsExpressionEvaluator.java
+++ arithmetics/src/main/java/com/kata/ArithmeticsExpressionEvaluator.java
@@ -1,9 +1,44 @@
 package com.kata;
 
+import java.util.Stack;
+
+import static java.lang.Character.*;
+
 public class ArithmeticsExpressionEvaluator {
 
     public int evaluate(String expression) {
-        final var tokens = RPNTokenizer.tokenize(expression);
-        return RPNCalculator.calculate(tokens);
+        final var numbers = new Stack<Integer>();
+        final var operators = new Stack<Character>();
+        if (expression.charAt(0) != '(')
+            throw new InvalidRecordException();
+        for (char c : expression.toCharArray()) {
+            if (c == ' ') {
+                continue;
+            } else if (isDigit(c)) {
+                numbers.push(getNumericValue(c));
+            } else if (c == ')') {
+                if (numbers.isEmpty()) {
+                    continue;
+                }
+                var b = numbers.pop();
+                var a = numbers.pop();
+                var op = operators.pop();
+                numbers.push(applyOp(a, b, op));
+                operators.pop();
+            } else {
+                operators.push(c);
+            }
+        }
+        return numbers.isEmpty() ? 0 : numbers.get(0);
+    }
+
+    private int applyOp(int x, int y, char op) {
+        return switch (op) {
+            case '+' -> x + y;
+            case '-' -> x - y;
+            case '*' -> x * y;
+            case '/' -> x / y;
+            default -> 0;
+        };
     }
 }
--- arithmetics/src/test/java/com/kata/ArithmeticsExpressionEvaluatorTest.java
+++ arithmetics/src/test/java/com/kata/ArithmeticsExpressionEvaluatorTest.java
@@ -3,7 +3,11 @@ package com.kata;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -17,6 +21,10 @@ class ArithmeticsExpressionEvaluatorTest {
         evaluator = new ArithmeticsExpressionEvaluator();
     }
 
+    private void assertEvaluation(int result, String expression) {
+        assertEquals(result, evaluator.evaluate(expression));
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"1", " 1 + (2 - 3)", "3 + (2 * 1)"})
     void noParamShouldThrow(String expression) {
@@ -26,19 +34,31 @@ class ArithmeticsExpressionEvaluatorTest {
     @ParameterizedTest
     @ValueSource(strings = {"()", "(())", "(()())"})
     void emptyBracesShouldReturn0(String expression) {
-        assertEquals(0, evaluator.evaluate(expression));
+        assertEvaluation(0, expression);
     }
 
     @Test
     void evaluateShouldSum() {
-        String expression = "(1 + 1)";
-        assertEquals(2, evaluator.evaluate(expression));
+        assertEvaluation(2, "(1 + 1)");
     }
 
     @Test
     void evaluateShouldMultiply() {
-        String expression = "(3 * 2)";
-        assertEquals(6, evaluator.evaluate(expression));
+        assertEvaluation(6, "(3 * 2)");
     }
 
+    @ParameterizedTest
+    @MethodSource("evaluateMultipleExpressionsSource")
+    void evaluateMultipleExpressions(int expected, String expression) {
+        assertEvaluation(expected, expression);
+    }
+
+    private static Stream<Arguments> evaluateMultipleExpressionsSource() {
+        return Stream.of(
+            Arguments.of(10, "(2 * (3 + 2))"),
+            Arguments.of(8, "(2 + (3 * 2))"),
+            Arguments.of(101, "(1 + ((2 + 3) * (4 * 5)))")
+//            Arguments.of(-165, "( 5 * ( 4 * ( 3 * ( 2 * ( 1 * 9 ) / 8 - 7 ) + 6 ) ) )")
+        );
+    }
 }
