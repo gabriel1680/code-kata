d2d308d46f9061b895cf3f1331adbf978ea0fa75
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-10-05 12:19:25 -0300

    feat(yatzy): add yatzy

--- yatzy/src/main/java/com/kata/Yatzy.java
+++ yatzy/src/main/java/com/kata/Yatzy.java
@@ -7,4 +7,8 @@ public class Yatzy {
     public int chance(List<Integer> rolls) {
         return rolls.stream().mapToInt(Integer::intValue).sum();
     }
+
+    public int yatzy(List<Integer> dices) {
+        return dices.stream().allMatch(dices.get(0)::equals) ? 50 : 0;
+    }
 }
--- yatzy/src/test/java/com/kata/YatzyTest.java
+++ yatzy/src/test/java/com/kata/YatzyTest.java
@@ -1,5 +1,6 @@
 package com.kata;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,10 +12,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class YatzyTest {
 
+    private Yatzy yatzy;
+
+    @BeforeEach
+    void setUp() {
+        yatzy = new Yatzy();
+    }
+
     @ParameterizedTest
     @MethodSource("chancesSource")
     void chanceShouldSumAllDices(List<Integer> dices, int result) {
-        final var yatzy = new Yatzy();
         assertEquals(result, yatzy.chance(dices));
     }
 
@@ -22,6 +29,23 @@ class YatzyTest {
         return Stream.of(
             Arguments.of(List.of(1, 1, 1, 1, 1), 5),
             Arguments.of(List.of(1, 2, 3, 4, 5), 15)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("yatzySource")
+    void yatzyShouldGet50WhenAllDicesAreEqualsOtherwise0(List<Integer> dices, int result) {
+        assertEquals(result, yatzy.yatzy(dices));
+    }
+
+    private static Stream<Arguments> yatzySource() {
+        return Stream.of(
+            Arguments.of(List.of(2, 2, 2, 2, 2), 50),
+            Arguments.of(List.of(1, 1, 1, 1, 1), 50),
+            Arguments.of(List.of(1, 1, 1, 1, 5), 0),
+            Arguments.of(List.of(1, 1, 1, 4, 5), 0),
+            Arguments.of(List.of(1, 1, 3, 4, 5), 0),
+            Arguments.of(List.of(1, 2, 3, 4, 5), 0)
         );
     }
 }
