3d865d02af6283f84c5d82886a5a425e122af23b
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-10-10 21:22:03 -0300

    refactor(mars-rover): add remove position and add direction

--- mars-rover/src/main/java/com/kata/Rover.java
+++ mars-rover/src/main/java/com/kata/Rover.java
@@ -2,23 +2,23 @@ package com.kata;
 
 public class Rover {
 
-    private String position;
+    private Direction direction;
     private int x;
     private int y;
 
     public Rover(int y, String position) {
         this.y = y;
-        this.position = position;
+        this.direction = Direction.valueOf(position);
     }
 
     public Rover(String aPosition) {
-        position = aPosition;
+        this.direction = Direction.valueOf(aPosition);
         y = 0;
         x = 0;
     }
 
     public Rover() {
-        position = "N";
+        this.direction = Direction.N;
         y = 0;
         x = 0;
     }
@@ -26,11 +26,11 @@ public class Rover {
     public Rover(int x, int y, String position) {
         this.x = x;
         this.y = y;
-        this.position = position;
+        this.direction = Direction.valueOf(position);
     }
 
     public String getPosition() {
-        return x + ":" + y + ":" + position;
+        return x + ":" + y + ":" + direction.value;
     }
 
     public void move(String commands) {
@@ -46,23 +46,56 @@ public class Rover {
     }
 
     private void moveForward() {
-        if (position == "N") y += 1;
-        else if (position == "S") y -= 1;
-        else if (position == "W") x -= 1;
+        if (direction.value == "N") y += 1;
+        else if (direction.value == "S") y -= 1;
+        else if (direction.value == "W") x -= 1;
         else x += 1;
     }
 
     private void turnLeft() {
-        if (position == "N") position = "W";
-        else if (position == "W") position = "S";
-        else if (position == "S") position = "E";
-        else position = "N";
+        if (direction.value == "N") {
+            direction = Direction.W;
+        }
+        else if (direction.value == "W") {
+            direction = Direction.S;
+        }
+        else if (direction.value == "S") {
+            direction = Direction.E;
+        }
+        else {
+            direction = Direction.N;
+        }
     }
 
     private void turnRight() {
-        if (position == "N") position = "E";
-        else if (position == "E") position = "S";
-        else if (position == "S") position = "W";
-        else position = "N";
+        if (direction.value == "N") {
+            direction = Direction.E;
+        }
+        else if (direction.value == "E") {
+            direction = Direction.S;
+        }
+        else if (direction.value == "S") {
+            direction = Direction.W;
+        }
+        else {
+            direction = Direction.N;
+        }
+    }
+
+    public enum Direction {
+        N("N", "W", "E"),
+        E("E", "N", "S"),
+        W("W", "S", "N"),
+        S("S", "E", "W");
+
+        public final String value;
+        public final String left;
+        public final String right;
+
+        Direction(String value, String left, String right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
