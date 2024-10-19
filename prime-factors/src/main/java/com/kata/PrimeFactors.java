26ff87116a19856119e62f5f1655eaa9c6eb9f0c
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-10-19 13:04:23 -0300

    refactor(prime-factors): green multiples of any

--- prime-factors/src/main/java/com/kata/PrimeFactors.java
+++ prime-factors/src/main/java/com/kata/PrimeFactors.java
@@ -6,15 +6,13 @@ import java.util.List;
 public class PrimeFactors {
     public List<Integer> of(int n) {
         final var result = new ArrayList<Integer>();
-        if (n > 1) {
-            while (n % 2 == 0) {
-                result.add(2);
-                n /= 2;
+        var divisor = 2;
+        while (n > 1) {
+            while (n % divisor == 0) {
+                result.add(divisor);
+                n /= divisor;
             }
-            while (n % 3 == 0) {
-                result.add(3);
-                n /= 3;
-            }
+            divisor++;
         }
         if (n > 1) {
             result.add(n);
