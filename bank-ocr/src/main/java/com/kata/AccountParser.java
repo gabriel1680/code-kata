ee5e79a9eb0cdf031bae1b925abc6e54561e6b38
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-11-10 15:42:30 -0300

    refactor(bank-ocr): extract functions

--- bank-ocr/src/main/java/com/kata/AccountParser.java
+++ bank-ocr/src/main/java/com/kata/AccountParser.java
@@ -7,13 +7,21 @@ import java.util.stream.IntStream;
 public class AccountParser {
 
     public AccountNumber parse(String content) {
-        final var accountNumberLines = List.of(content.split("\n"));
-        final var digits = IntStream.iterate(0, i -> i + 3)
+        final var accountNumberLines = getAccountLinesFrom(content);
+        final var digits = getDigitsFrom(accountNumberLines);
+        return new AccountNumber(digits);
+    }
+
+    private static List<String> getAccountLinesFrom(String content) {
+        return List.of(content.split("\n"));
+    }
+
+    private List<Digit> getDigitsFrom(List<String> accountNumberLines) {
+        return IntStream.iterate(0, i -> i + 3)
                 .limit(9)
                 .mapToObj(i -> toNumberString(accountNumberLines, i))
                 .map(Digit::of)
                 .toList();
-        return new AccountNumber(digits);
     }
 
     public String toNumberString(List<String> lines, int n) {
