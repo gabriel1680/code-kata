35de59ac325f3ed039d1b6122869fabf445a5ab1
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-11-11 22:08:53 -0300

    refactor(bank-ocr): replace for loop with stream channing on banking

--- bank-ocr/src/main/java/com/kata/BankOCR.java
+++ bank-ocr/src/main/java/com/kata/BankOCR.java
@@ -5,19 +5,21 @@ public class BankOCR {
     private final FileReader fileReader;
     private final IO io;
     private final AccountNumberPresenter presenter;
+    private final OCRFileParser fileParser;
 
     public BankOCR(FileReader fileReader, IO io, AccountNumberPresenter presenter) {
         this.fileReader = fileReader;
         this.io = io;
         this.presenter = presenter;
+        this.fileParser = new OCRFileParser();
     }
 
     public void run() {
         final var filepath = io.read();
         final var content = fileReader.read(filepath);
-        final var fileParser = new OCRFileParser();
-        for (var accountNumber : fileParser.parse(content)) {
-            io.print(presenter.present(accountNumber));
-        }
+        final var fileParser = this.fileParser;
+        fileParser.parse(content).stream()
+                .map(presenter::present)
+                .forEach(io::print);
     }
 }
