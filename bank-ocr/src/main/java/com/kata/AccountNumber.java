db3474befed27361faf0023190d66c9086a651dc
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-11-20 14:43:38 -0300

    feat(bank-ocr): add guessing checksum combination

--- bank-ocr/src/main/java/com/kata/AccountNumber.java
+++ bank-ocr/src/main/java/com/kata/AccountNumber.java
@@ -1,15 +1,74 @@
 package com.kata;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 
 public record AccountNumber(List<Integer> digits) {
 
     public boolean checksum() {
+        return calculateChecksum(digits);
+    }
+
+    private boolean calculateChecksum(List<Integer> digits) {
         final int checksum = IntStream
                 .range(0, digits.size())
                 .map(i -> digits.get(i) * (i - digits.size()))
                 .sum();
         return checksum % 11 == 0;
+    }
+
+    public List<String> getPossibleCombinations() {
+        if (checksum()) {
+            return singletonList(digitsToString(digits));
+        }
+        List<List<String>> result = new ArrayList<>();
+        for (var digit : removeDuplicationsFor(digits)) {
+            result.add(getProximalOf(digit).stream()
+                    .map(this::getOneDigitsCombinations)
+                    .flatMap(Collection::stream)
+                    .toList());
+        }
+        return result.stream()
+                .flatMap(Collection::stream)
+                .toList();
+
+    }
+
+    private List<Integer> removeDuplicationsFor(List<Integer> aList) {
+        return new ArrayList<>(new HashSet<>(aList));
+    }
+
+    private List<String> getOneDigitsCombinations(int element) {
+        final List<String> result = new ArrayList<>();
+        for (int i = 0; i < digits.size(); i++) {
+            var mutableDigits = new ArrayList<>(digits);
+            mutableDigits.set(i, element);
+            if (calculateChecksum(mutableDigits))
+                result.add(digitsToString(mutableDigits));
+        }
+        return result;
+    }
+
+    private String digitsToString(List<Integer> list) {
+        return list.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    }
+
+    private static List<Integer> getProximalOf(int n) {
+        return switch (n) {
+            case 5 -> List.of(6, 9);
+            case 6 -> List.of(5, 8);
+            case 8 -> List.of(6, 0, 9);
+            case 9 -> List.of(8, 3, 5);
+            default -> emptyList();
+        };
     }
 }
--- bank-ocr/src/test/java/com/kata/AccountNumberTest.java
+++ bank-ocr/src/test/java/com/kata/AccountNumberTest.java
@@ -21,4 +21,27 @@ class AccountNumberTest {
         final var accountNumber = new AccountNumber(digits);
         assertThat(accountNumber.checksum()).isFalse();
     }
+
+    @Test
+    void guessTheValidChecksum() {
+        final var validChecksumDigits = List.of(3, 4, 5, 8, 8, 2, 8, 6, 5);
+        var accountNumber = new AccountNumber(validChecksumDigits);
+        assertThat(accountNumber.getPossibleCombinations()).containsExactly("345882865");
+
+        var invalidChecksumDigits = List.of(5, 5, 5, 5, 5, 5, 5, 5, 5);
+        accountNumber = new AccountNumber(invalidChecksumDigits);
+        assertThat(accountNumber.getPossibleCombinations()).contains("555655555", "559555555");
+
+        invalidChecksumDigits = List.of(6, 6, 6, 6, 6, 6, 6, 6, 6);
+        accountNumber = new AccountNumber(invalidChecksumDigits);
+        assertThat(accountNumber.getPossibleCombinations()).contains("666566666", "686666666");
+
+        invalidChecksumDigits = List.of(8, 8, 8, 8, 8, 8, 8, 8, 8);
+        accountNumber = new AccountNumber(invalidChecksumDigits);
+        assertThat(accountNumber.getPossibleCombinations()).contains("888886888", "888888880", "888888988");
+
+        invalidChecksumDigits = List.of(9, 9, 9, 9, 9, 9, 9, 9, 9);
+        accountNumber = new AccountNumber(invalidChecksumDigits);
+        assertThat(accountNumber.getPossibleCombinations()).contains("899999999", "993999999", "999959999");
+    }
 }
