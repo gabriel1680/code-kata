08d2ac6dbe8535791224be92b685d441def1d0bf
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-11-23 23:07:02 -0300

    refactor(bank-ocr): move parsing digit

--- bank-ocr/src/main/java/com/kata/AccountNumberPresenter.java
+++ bank-ocr/src/main/java/com/kata/AccountNumberPresenter.java
@@ -6,33 +6,36 @@ import java.util.stream.Collectors;
 public class AccountNumberPresenter {
 
     public String present(AccountNumber accountNumber) {
-        final var digits = accountNumber.digits().stream()
-                .map(String::valueOf)
-                .map(AccountNumberPresenter::replaceInvalidDigits)
-                .collect(Collectors.joining());
         return present(
-                digits,
+                accountNumber.digits(),
                 accountNumber.checksum(),
                 accountNumber.getPossibleCombinations());
     }
 
-    private static String replaceInvalidDigits(String digit) {
-        return digit.equals("-1") ? "?" : digit;
-    }
-
-    private static String present(String digits, boolean checksum, List<String> possibleChecksum) {
+    private static String present(List<Integer> digits, boolean checksum, List<String> possibleChecksum) {
         if (isGuessedVariantCorrect(checksum, possibleChecksum)) {
             return possibleChecksum.get(0);
         }
-        return digits + getStatus(digits, checksum, possibleChecksum);
+        return presentDigits(digits) + getStatus(digits, checksum, possibleChecksum);
     }
 
     private static boolean isGuessedVariantCorrect(boolean checksum, List<String> possibleChecksum) {
         return !checksum && possibleChecksum.size() == 1;
     }
 
-    private static String getStatus(String digits, boolean checksum, List<String> possibleChecksum) {
-        if (digits.contains("?")) {
+    private static String presentDigits(List<Integer> digits) {
+        return digits.stream()
+                .map(String::valueOf)
+                .map(AccountNumberPresenter::replaceInvalidDigits)
+                .collect(Collectors.joining());
+    }
+
+    private static String replaceInvalidDigits(String digit) {
+        return digit.equals("-1") ? "?" : digit;
+    }
+
+    private static String getStatus(List<Integer> digits, boolean checksum, List<String> possibleChecksum) {
+        if (digits.contains(-1)) {
             return " ILL";
         } else if (!checksum) {
             return possibleChecksum.size() > 1 ? " AMB " + possibleChecksum : " ILL";
