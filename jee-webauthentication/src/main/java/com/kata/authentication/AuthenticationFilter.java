8f3ec3488742bd08019d9556c09f1bbc31b3d6e0
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-03-03 15:28:44 -0300

    feat(jee-webauth): add sso token validation

--- jee-webauthentication/src/main/java/com/kata/authentication/AuthenticationFilter.java
+++ jee-webauthentication/src/main/java/com/kata/authentication/AuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.kata.authentication;
 
 import com.kata.LdapAuthenticationGateway;
+import com.kata.SingleSignOnRegistry;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,17 +9,25 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
 public class AuthenticationFilter implements Filter {
 
+    private static final String COOKIE_TOKEN = "x-sso-token";
+
     private final LdapAuthenticationGateway authenticationGateway;
+    private final SingleSignOnRegistry ssoRegistry;
     private final PostCredentialParser postCredentialParser;
     private final GetCredentialParser getCredentialParser;
 
-    public AuthenticationFilter(LdapAuthenticationGateway authenticationGateway) {
+    public AuthenticationFilter(LdapAuthenticationGateway authenticationGateway, SingleSignOnRegistry ssoRegistry) {
         this.authenticationGateway = authenticationGateway;
+        this.ssoRegistry = ssoRegistry;
         this.postCredentialParser = new PostCredentialParser();
         this.getCredentialParser = new GetCredentialParser();
     }
@@ -29,12 +38,37 @@ public class AuthenticationFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
-        final var method = ((HttpServletRequest) request).getMethod();
-        final var requestParser = getRequestParser(method);
-        final var credentials = requestParser.getCredentialsOf(request);
+        validateToken((HttpServletRequest) request);
+        final var credentials = validateCredentials(request);
+        registerNewSession((HttpServletResponse) response, credentials);
+        chain.doFilter(request, response);
+    }
+
+    private void validateToken(HttpServletRequest request) {
+        getAuthTokenFrom(request).ifPresent(token -> {
+            if (!ssoRegistry.tokenIsValid(token))
+                throw new RuntimeException("invalid token");
+        });
+    }
+
+    private Credentials validateCredentials(ServletRequest request) {
+        final var credentials = getCredentialsFrom(request);
         if (!authenticationGateway.credentialsAreValid(credentials.username(), credentials.password()))
             throw new RuntimeException("invalid username or password");
-        chain.doFilter(request, response);
+        return credentials;
+    }
+
+    private Credentials getCredentialsFrom(ServletRequest request) {
+        final var method = ((HttpServletRequest) request).getMethod();
+        final var requestParser = getRequestParser(method);
+        return requestParser.getCredentialsOf(request);
+    }
+
+    private static Optional<String> getAuthTokenFrom(HttpServletRequest request) {
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> COOKIE_TOKEN.equals(cookie.getName()))
+                .findFirst()
+                .map(Cookie::getValue);
     }
 
     private RequestCredentialParser getRequestParser(String method) {
@@ -43,6 +77,11 @@ public class AuthenticationFilter implements Filter {
             case "GET" -> getCredentialParser;
             default -> throw new RuntimeException("invalid request method");
         };
+    }
+
+    private void registerNewSession(HttpServletResponse response, Credentials credentials) {
+        final var token = ssoRegistry.registerNewSession(credentials.username());
+        response.addCookie(new Cookie(COOKIE_TOKEN, token));
     }
 
     @Override
--- jee-webauthentication/src/test/java/com/kata/AuthenticationFilterTest.java
+++ jee-webauthentication/src/test/java/com/kata/AuthenticationFilterTest.java
@@ -6,17 +6,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
@@ -34,6 +38,7 @@ class AuthenticationFilterTest {
     FilterChain chain;
 
     LdapAuthenticationGateway authenticationGateway;
+    SingleSignOnRegistry ssoRegistry;
     AuthenticationFilter sut;
 
     static class FakeAuthenticationGateway implements LdapAuthenticationGateway {
@@ -46,7 +51,9 @@ class AuthenticationFilterTest {
     @BeforeEach
     void setUp() {
         authenticationGateway = new FakeAuthenticationGateway();
-        sut = new AuthenticationFilter(authenticationGateway);
+        ssoRegistry = new FakeSSORegistry();
+        sut = new AuthenticationFilter(authenticationGateway, ssoRegistry);
+        when(request.getCookies()).thenReturn(new Cookie[]{});
     }
 
     @Test
@@ -120,6 +127,55 @@ class AuthenticationFilterTest {
         when(request.getMethod()).thenReturn("POST");
         when(request.getContentType()).thenReturn("application/json");
         when(request.getReader()).thenReturn(new BufferedReader(new StringReader("{\"username\":\"John Doe\",\"password\":\"john_doe123\"}")));
+        sut.doFilter(request, response, chain);
+        verify(chain, times(1)).doFilter(any(), any());
+    }
+
+    static class FakeSSORegistry implements SingleSignOnRegistry {
+
+
+        @Override
+        public boolean tokenIsValid(String token) {
+            return "1".equals(token);
+        }
+
+        @Override
+        public String registerNewSession(String userName) {
+            return userName.equals("John Doe") ? "1" : "2";
+        }
+
+        @Override
+        public void endSession(String token) {
+        }
+    }
+
+    @Captor
+    ArgumentCaptor<Cookie> cookieCaptor;
+
+    @Test
+    void afterSuccessfullyLogin_shouldRegisterTheSessionOnACookie() throws ServletException, IOException {
+        when(request.getMethod()).thenReturn("GET");
+        when(request.getParameter("username")).thenReturn("John Doe");
+        when(request.getParameter("password")).thenReturn("john_doe123");
+        sut.doFilter(request, response, chain);
+        verify(response, times(1)).addCookie(cookieCaptor.capture());
+        final var cookie = cookieCaptor.getValue();
+        assertThat(cookie.getName()).isEqualTo("x-sso-token");
+        assertThat(cookie.getValue()).isEqualTo("1");
+    }
+
+    @Test
+    void cookieWithInvalidSSOSessionId() {
+        when(request.getCookies()).thenReturn(new Cookie[]{new Cookie("x-sso-token", "5")});
+        assertThatThrownBy(() -> sut.doFilter(request, response, chain)).hasMessage("invalid token");
+    }
+
+    @Test
+    void cookieWithValidSSOSessionId() throws ServletException, IOException {
+        when(request.getMethod()).thenReturn("GET");
+        when(request.getParameter("username")).thenReturn("John Doe");
+        when(request.getParameter("password")).thenReturn("john_doe123");
+        when(request.getCookies()).thenReturn(new Cookie[]{new Cookie("x-sso-token", "1")});
         sut.doFilter(request, response, chain);
         verify(chain, times(1)).doFilter(any(), any());
     }
