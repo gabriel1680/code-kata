bcb1fc7945f920621dd959d823775969682e826c
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-03-03 01:26:01 -0300

    refactor(jee-webauth): extract methods and better interfaces

--- jee-webauthentication/src/main/java/com/kata/AuthenticationFilter.java
+++ jee-webauthentication/src/main/java/com/kata/AuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.kata;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.Filter;
@@ -11,34 +12,38 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Optional;
 
 public class AuthenticationFilter implements Filter {
 
-    private final PostAuthenticationFilter postAuthenticationFilter;
-    private final GetAuthenticationFilter getAuthenticationFilter;
+    private final LdapAuthenticationGateway authenticationGateway;
+    private final PostCredentialParser postCredentialParser;
+    private final GetCredentialParser getCredentialParser;
 
     public AuthenticationFilter(LdapAuthenticationGateway authenticationGateway) {
-        postAuthenticationFilter = new PostAuthenticationFilter(authenticationGateway);
-        getAuthenticationFilter = new GetAuthenticationFilter(authenticationGateway);
+        this.authenticationGateway = authenticationGateway;
+        this.postCredentialParser = new PostCredentialParser();
+        this.getCredentialParser = new GetCredentialParser();
     }
 
     @Override
     public void init(FilterConfig config) {
-        getAuthenticationFilter.init(config);
-        postAuthenticationFilter.init(config);
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         final var method = ((HttpServletRequest) request).getMethod();
-        final var authenticator = getAuthenticator(method);
-        authenticator.doFilter(request, response, chain);
+        final var requestParser = getRequestParser(method);
+        final var credentials = requestParser.getCredentialsOf(request);
+        if (!authenticationGateway.credentialsAreValid(credentials.username, credentials.password))
+            throw new RuntimeException("invalid username or password");
+        chain.doFilter(request, response);
     }
 
-    private Filter getAuthenticator(String method) {
+    private RequestCredentialParser getRequestParser(String method) {
         return switch (method) {
-            case "POST" -> postAuthenticationFilter;
-            case "GET" -> getAuthenticationFilter;
+            case "POST" -> postCredentialParser;
+            case "GET" -> getCredentialParser;
             default -> throw new RuntimeException("invalid request method");
         };
     }
@@ -46,61 +51,53 @@ public class AuthenticationFilter implements Filter {
 
     @Override
     public void destroy() {
-        getAuthenticationFilter.destroy();
-        postAuthenticationFilter.destroy();
     }
 
-    private record PostAuthenticationFilter(LdapAuthenticationGateway authenticationGateway) implements Filter {
+    private interface RequestCredentialParser {
+        Credentials getCredentialsOf(ServletRequest request);
+    }
 
-        @Override
-        public void init(FilterConfig filterConfig) {
-        }
+    private record Credentials(String username, String password) {
+    }
 
+    private static class PostCredentialParser implements RequestCredentialParser {
         @Override
-        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        public Credentials getCredentialsOf(ServletRequest request) {
             if (!request.getContentType().equals("application/json"))
                 throw new RuntimeException("invalid request content type");
             try (BufferedReader reader = request.getReader()) {
-                final var objectMapper = new ObjectMapper();
-                final var jsonNode = objectMapper.readTree(reader);
-                final var usernameJsonNode = jsonNode.get("username");
-                final var passwordJsonNode = jsonNode.get("password");
-                if (usernameJsonNode == null || usernameJsonNode.asText("").isEmpty() || passwordJsonNode == null || passwordJsonNode.asText("").isEmpty())
-                    throw new RuntimeException("invalid request format");
-                final var userName = usernameJsonNode.asText();
-                final var password = passwordJsonNode.asText();
-                if (!authenticationGateway.credentialsAreValid(userName, password))
-                    throw new RuntimeException("invalid username or password");
-                chain.doFilter(request, response);
+                return getCredentialsOf(reader);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
 
-        @Override
-        public void destroy() {
+        private static Credentials getCredentialsOf(BufferedReader reader) throws IOException {
+            final var objectMapper = new ObjectMapper();
+            final var jsonNode = objectMapper.readTree(reader);
+            final var username = getOrThrowProperty(jsonNode.get("username"));
+            final var password = getOrThrowProperty(jsonNode.get("password"));
+            return new Credentials(username, password);
+        }
+
+        private static String getOrThrowProperty(JsonNode key) {
+            return Optional.ofNullable(key)
+                    .map(JsonNode::asText)
+                    .filter(i -> !i.isEmpty())
+                    .orElseThrow(() -> new RuntimeException("invalid request format"));
         }
     }
 
-    private record GetAuthenticationFilter(LdapAuthenticationGateway authenticationGateway) implements Filter {
-
+    private static class GetCredentialParser implements RequestCredentialParser {
         @Override
-        public void init(FilterConfig filterConfig) {
-        }
-
-        @Override
-        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        public Credentials getCredentialsOf(ServletRequest request) {
             final var username = request.getParameter("username");
             if (username == null || username.isEmpty())
                 throw new RuntimeException("invalid request missing username");
             final var password = request.getParameter("password");
             if (password == null || password.isEmpty())
                 throw new RuntimeException("invalid request missing password");
-            if (!authenticationGateway.credentialsAreValid(username, password))
-                throw new RuntimeException("invalid username or password");
-            chain.doFilter(request, response);
-        }
-
-        @Override
-        public void destroy() {
+            return new Credentials(username, password);
         }
     }
 }
