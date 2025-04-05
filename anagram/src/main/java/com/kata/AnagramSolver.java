206c37df86f0797462bd7e3196cefcc19edb7811
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-04-05 18:53:32 -0300

    feat: add acceptance test

--- anagram/src/main/java/com/kata/AnagramSolver.java
+++ anagram/src/main/java/com/kata/AnagramSolver.java
@@ -1,24 +1,4 @@
 package com.kata;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class AnagramSolver {
-
-    public List<String> solve(String word, String[] words) {
-        return Arrays.stream(words)
-                .filter(aWord -> isAnagram(word, aWord))
-                .toList();
-    }
-
-    private static boolean isAnagram(String a, String b) {
-        if (a.length() != b.length()) return false;
-        return Arrays.equals(toSortedCharArray(a), toSortedCharArray(b));
-    }
-
-    private static char[] toSortedCharArray(String word) {
-        char[] chars = word.toCharArray();
-        Arrays.sort(chars);
-        return chars;
-    }
 }
---
+++ anagram/src/test/java/com/kata/IntegrationTest.java
@@ -0,0 +1,35 @@
+package com.kata;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.inOrder;
+
+@ExtendWith(MockitoExtension.class)
+class IntegrationTest {
+
+    @Mock
+    private PrintStream out;
+
+    private Anagram anagram;
+
+    @BeforeEach
+    void setUp() {
+        anagram = new Anagram();
+    }
+
+    @Test
+    void acceptance() {
+        anagram.run();
+        InOrder inOrder = inOrder(out);
+        inOrder.verify(out).println("documenting");
+        inOrder.verify(out).println("gnitnemucod");
+        inOrder.verify(out).println("mnietgnuodc");
+    }
+}
