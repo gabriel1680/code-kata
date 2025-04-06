e04c63806aaa2668fc6d6412e147eaf6a3922cbe
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-04-06 12:03:28 -0300

    feat(anagram): add Main file and anagrams.txt

--- anagram/src/main/java/com/kata/Main.java
+++ anagram/src/main/java/com/kata/Main.java
@@ -1,7 +1,17 @@
 package com.kata;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException {
+        final var path = Paths.get("src", "main", "resources", "anagrams.txt");
+        final var reader = Files.newBufferedReader(path);
+        final var provider = new AnagramsProvider(reader);
+        final var solver = new AnagramSolver();
+        final var anagram = new Anagram(System.out, provider, solver);
+        anagram.run(args[0]);
     }
 }
