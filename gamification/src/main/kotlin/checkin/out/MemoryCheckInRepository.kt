97d0c6f0714f2a74a47a38d47f029ac7b24b5669
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-11-22 18:40:21 -0300

    test: refactor tests with fakes

--- gamification/src/main/kotlin/checkin/out/MemoryCheckInRepository.kt
+++ gamification/src/main/kotlin/checkin/out/MemoryCheckInRepository.kt
@@ -4,6 +4,7 @@ import org.gbl.checkin.application.domain.CheckInMissionRepository
 import org.gbl.checkin.application.domain.DailyCheckInMission
 
 class MemoryCheckInRepository : CheckInMissionRepository {
+
     private val missions = mutableMapOf<Long, DailyCheckInMission>()
 
     override fun getFor(userId: Long) = missions.get(userId)
--- gamification/src/test/kotlin/checkin/app/usecase/CheckInTest.kt
+++ gamification/src/test/kotlin/checkin/app/usecase/CheckInTest.kt
@@ -1,50 +1,51 @@
 package checkin.app.usecase
 
+import checkin.app.fixture.MockClock
+import org.gbl.checkin.app.usecase.CheckIn
 import org.gbl.checkin.app.usecase.CheckInInput
-import org.gbl.checkin.application.domain.CheckIn
-import org.gbl.checkin.application.domain.CheckInMissionRepository
 import org.gbl.checkin.application.domain.DailyCheckInMission
+import org.gbl.checkin.out.MemoryCheckInRepository
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.*
-import java.time.Clock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.verify
 import java.time.Duration
 import java.time.Instant
+import kotlin.test.assertEquals
 
 class CheckInTest {
 
     companion object Fixture {
         private const val USER_ID = 123L
         private val NOW = Instant.parse("2025-11-15T00:00:00Z")
-        private val A_COMMAND = CheckInInput(USER_ID)
+        private val INPUT = CheckInInput(USER_ID)
+    }
+
+    private lateinit var clock: MockClock
+    private lateinit var repository: MemoryCheckInRepository
+    private lateinit var checkIn: CheckIn
+
+    @BeforeEach
+    fun setUp() {
+        repository = MemoryCheckInRepository()
+        clock = MockClock(NOW)
+        checkIn = CheckIn(repository, clock)
     }
 
     @Test
     fun `first check-in of a user`() {
-        val repository = mock<CheckInMissionRepository> {
-            on { getFor(any()) } doReturn null
-        }
-        val clock = mock<Clock> {
-            on { instant() } doReturn NOW
-        }
-        val act = org.gbl.checkin.app.usecase.CheckIn(repository, clock)
-        act(A_COMMAND)
-        verify(repository).save(any<DailyCheckInMission>())
+        checkIn(INPUT)
+       assertEquals(1, repository.getFor(USER_ID)!!.checkIns().size)
     }
 
     @Test
     fun `second check-in of a user`() {
-        val mission = DailyCheckInMission(USER_ID, listOf(CheckIn.first(NOW)))
-        val repository = mock<CheckInMissionRepository> {
-            on { getFor(any()) } doReturn mission
-        }
+        val mission = DailyCheckInMission.start(USER_ID)
+        mission.checkIn(NOW)
+        repository.save(mission)
         val dayAfterFirstCheckIn = NOW.plus(Duration.ofDays(1))
-        val clock = mock<Clock> {
-            on { instant() } doReturn dayAfterFirstCheckIn
-        }
-        val act = org.gbl.checkin.app.usecase.CheckIn(repository, clock)
-        act(A_COMMAND)
-        val captor = argumentCaptor<DailyCheckInMission> { }
-        verify(repository).save(captor.capture())
-        kotlin.test.assertEquals(2, captor.firstValue.checkIns().size)
+        clock.withInstant(dayAfterFirstCheckIn)
+        checkIn(INPUT)
+        assertEquals(2, mission.checkIns().size)
     }
 }
